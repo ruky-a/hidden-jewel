8bdf304c9358c75ade28a6b7a820ba0fa16909b7
ruky-a <rukevwe2006@gmail.com>
2020-02-09 00:41:29 -0500

    Devise

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,11 +6,12 @@ class HomepageController < ApplicationController
   def index
    @categories = Category.limit(4)
     @places = Place.limit(3)
+
   end
 
 def search 
    @categories = Category.all
-   @places = Place.search(params)
+  @places = Place.search(params)
   @places = Place.where(category_id: params[:category].to_i)
   @places = Place.where("name like ? or description like ?", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
   @places = Place.near(params[:location], 20) if params[:location].present?
